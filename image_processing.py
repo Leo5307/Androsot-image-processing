8cbfbbdd8ede3192d93cdf36b4797f9a461f1b92
Leo5307 <inquisitorleo0123@gmail.com>
2024-04-03 21:39:24 +0800

    Refactor: refactor ./image_processing

--- image_processing.py
+++ image_processing.py
@@ -3,7 +3,7 @@ import time
 import cv2
 import cv2.aruco as aruco
 import math
-from Aruco_distance_estimation_multi_target import my_estimatePoseSingleMarkers,calc_Aruco_distance,rotation_mtx2euler_angle,get_Aruco_information
+from Robot_pos_estimator import my_estimatePoseSingleMarkers,calc_Aruco_distance,rotation_mtx2euler_angle,get_Aruco_information
 from color_masking import colorMasking
 ARUCO_SIZE = 0.08#0.053
 CAMERA_HEIGHT = 1.5
@@ -12,32 +12,65 @@ FIELD_LENGTH = 3.6
 FIELD_WIDTH = 2.6
 
 class Robot():
-    def __init__(self,id,x,y,degree): 
-        self.id_list = id
-        self.x_list = x
-        self.y_list = y
-        self.degree_list = degree
+    def __init__(self,id,aruco_id_list,aruco_x_list,aruco_y_list,aruco_degree_list):
+        self.id = id 
+        self.aruco_id_list = aruco_id_list
+        self.aruco_x_list = aruco_x_list
+        self.aruco_y_list = aruco_y_list
+        self.aruco_degree_list = aruco_degree_list
         self.x = 0
         self.y = 0
         self.degree = 0
-        self.robot_pose_estimation()
     
     def robot_pose_estimation(self):
-        for index,id in enumerate(self.id_list):
-            if(id == 1 or id == 0):
-                self.x = FIELD_LENGTH*100 - (self.x_list[index] - CAMERA_DISTANCE*100)
-                self.y = (FIELD_WIDTH*100/2 - self.y_list[index])
-                self.degree = self.degree_list[index]
+        for index,id in enumerate(self.aruco_id_list):
+            if(id == 1 or id == 0):#,3,5,7,9,clockwise
+                self.x = FIELD_LENGTH* 100 - (self.aruco_x_list[index] - CAMERA_DISTANCE*100)
+                self.y = (FIELD_WIDTH* 100/2 - self.aruco_y_list[index])
+                self.degree = self.aruco_degree_list[index]
+                
+            # elif(id == 3 or id == 2):
+
+
+
+    def update_robot_position(self,id,aruco_id_list,aruco_x_list,aruco_y_list,aruco_degree_list):
+        self.id = id
+        self.aruco_id_list = aruco_id_list
+        self.aruco_x_list = aruco_x_list
+        self.aruco_y_list = aruco_y_list
+        self.aruco_degree_list = aruco_degree_list
+        self.robot_pose_estimation()
+    
+        
+
 
 class Robot_Team():
-    def __init__(self,color):
+    def __init__(self,color,length):
         self.color = color
+        self.team_length = length
         self.Robot_list = []
+        for i in range(length):
+            aruco_id_list,aruco_x_list,aruco_y_list,aruco_degree_list = [],[],[],[]
+            self.Robot_list.append(Robot(i,aruco_id_list,aruco_x_list,aruco_y_list,aruco_degree_list))
+
+    def update_position(self,aruco_id_list,aruco_x_list,aruco_y_list,aruco_degree_list):
+        for robot_id in range(self.team_length):
+            slice_aruco_id_list,slice_aruco_x_list,slice_aruco_y_list,slice_aruco_degree_list = [],[],[],[]
+            for index,aruco_id in enumerate(aruco_id_list):
+                if aruco_id >= 10*(robot_id) and aruco_id < 10*(robot_id + 1):
+                    slice_aruco_id_list.append(aruco_id_list[index])
+                    slice_aruco_x_list.append(aruco_x_list[index])
+                    slice_aruco_y_list.append(aruco_y_list[index])
+                    slice_aruco_degree_list.append(aruco_degree_list[index])
+
+                self.Robot_list[robot_id].update_robot_position(robot_id,slice_aruco_id_list,slice_aruco_x_list,slice_aruco_y_list,slice_aruco_degree_list)
+
 
     def get_information(self):
         print("Team:",self.color," num:",len(self.Robot_list),"\n")
         for robot in self.Robot_list:
-            print("robot:"," x:",robot.x," y:",robot.y," degree",robot.degree)
+            print("robot:",robot.id," x:",robot.x," y:",robot.y," degree",robot.degree)
+            
             
 def gamma_correction(frame, gamma=1.0):
 	# build a lookup table mapping the pixel values [0, 255] to
@@ -62,13 +95,21 @@ def findball(frame):
             ball_y = int(yy)
             center = [ball_x,ball_y]
             radius = int(radius)
+            
             if radius > 20:
                 cv2.circle(frame, center, radius, (0,255,0), 3)  # 畫圓
                 cv2.putText(frame, "Ball", (x, y - 5), font, 0.7, (0,255,0), 2)  # 螢幕上寫出"Ball"
                 datacenter.append(center)  # 儲存球心座標
-                dataradius.append(radius)  # 儲存半徑座標`
+                dataradius.append(radius)  # 儲存半徑座標
+                
                 # print(center)
-    return datacenter, dataradius
+    #只取第一個
+    ballcenter = datacenter[0]
+    transform_ball_x = int((ballcenter[0] / 1920) * FIELD_LENGTH * 100)
+    transform_ball_y = int((ballcenter[1] / 1080) * FIELD_WIDTH * 100)
+    ballcenter = [transform_ball_x,transform_ball_y]
+    
+    return ballcenter, dataradius
  
 if __name__ == '__main__':
     calibration_matrix_path = "./calibration_matrix.npy"
@@ -77,12 +118,14 @@ if __name__ == '__main__':
     dist_matrix = np.load(distortion_coefficients_path)
 
     cap = cv2.VideoCapture(1,cv2.CAP_DSHOW) 
-    cap = cv2.VideoCapture('2024-03-27_19-16-30.mp4') # 讀取電腦中的影片
+    cap = cv2.VideoCapture('2024-03-27_19-19-03.mp4') # 讀取電腦中的影片
 
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
     cap.set(cv2.CAP_PROP_FPS, 60)  # 设置帧率为60帧/秒
     font = cv2.FONT_HERSHEY_SIMPLEX #font for displaying text (below)
+    red_team = Robot_Team(color='red',length = 2)
+    blue_team = Robot_Team(color='blue',length = 2)
 
     if not cap.isOpened():
         print("Cannot open camera")
@@ -115,9 +158,8 @@ if __name__ == '__main__':
 
         #使用aruco.detectMarkers()函數可以檢測到marker，返回ID和标志板的4个角点坐標
         corners, ids, rejectedImgPoints = detector.detectMarkers(gray)
-        frame,id_list,distance_list,distance_x_list,degree_list = get_Aruco_information(frame=frame,corners=corners,ids=ids)
-        red_team = Robot_Team(color='red')
-        blue_team = Robot_Team(color='blue')
+        frame,id_list,distance_list,distance_x_list,degree_list,degree_list_x,degree_list_y,degree_list_z\
+              = get_Aruco_information(camera_matrix=camera_matrix,dist_matrix=dist_matrix,frame=frame,corners=corners,ids=ids)
 
         red_id_list = []
         blue_id_list = []
@@ -145,9 +187,11 @@ if __name__ == '__main__':
                 # red_team.Robot_list.append(Robot(id=id,x=distance_x_list[index],y=distance_list[index],degree=degree_list[index]))
                 # print(id)
                 # red_team.Robot_list.append(Robot(id=id,x=distance_list[index],y=distance_x_list[index],degree=degree_list[index]))
-
-        blue_team.Robot_list.append(Robot(id=blue_id_list,x=blue_x_list,y=blue_y_list,degree=blue_degree_list))
-        red_team.Robot_list.append(Robot(id=red_id_list,x=red_x_list,y=red_y_list,degree=red_degree_list))
+        #aruco_id_list,aruco_x_list,aruco_y_list,aruco_degree_list
+        blue_team.update_position(blue_id_list,blue_x_list,blue_y_list,blue_degree_list)
+        red_team.update_position(red_id_list,red_x_list,red_y_list,red_degree_list)
+        # blue_team.Robot_list.append(Robot(id=blue_id_list,x=blue_x_list,y=blue_y_list,degree=blue_degree_list))
+        # red_team.Robot_list.append(Robot(id=red_id_list,x=red_x_list,y=red_y_list,degree=red_degree_list))
 
         # red_team.get_information()
         blue_team.get_information()
@@ -155,14 +199,14 @@ if __name__ == '__main__':
 
 
         # frame_resized = cv2.resize(frame, (0, 0), fx=1000, fy=800)
-        frame = cv2.warpPerspective(frame, m, (1080, 1920))
-        frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        # frame = cv2.warpPerspective(frame, m, (1080, 1920))
+        # frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
         ballcenter, ballradius = findball(frame)
-        ballcenter = ballcenter[0]
-        ballcenter = [int((ballcenter[0]/1920)*FIELD_LENGTH*100),int((ballcenter[1]/1080)*FIELD_WIDTH*100)]
-        cv2.putText(frame,'coordinates:'+ str([blue_team.Robot_list[0].x,blue_team.Robot_list[0].y]) ,(0, 110), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
-        cv2.putText(frame,'deg_z:'+ str(blue_team.Robot_list[0].degree) ,(0, 150), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
-        cv2.putText(frame,'ballcenter:'+ str(ballcenter) ,(0, 190), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+        # ballcenter = ballcenter[0]
+        # ballcenter = [int((ballcenter[0]/1920)*FIELD_LENGTH*100),int((ballcenter[1]/1080)*FIELD_WIDTH*100)]
+        # cv2.putText(frame,'coordinates:'+ str([blue_team.Robot_list[0].x,blue_team.Robot_list[0].y]) ,(0, 110), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+        # cv2.putText(frame,'deg_z:'+ str(blue_team.Robot_list[0].degree) ,(0, 150), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+        # cv2.putText(frame,'ballcenter:'+ str(ballcenter) ,(0, 190), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
         cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
         cv2.resizeWindow('frame', 1200, 800)
         cv2.imshow("frame",frame)
