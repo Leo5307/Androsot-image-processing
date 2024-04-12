bee1037b1e69e624a142811a6ca8b9294efa6500
Leo5307 <inquisitorleo0123@gmail.com>
2024-04-12 17:21:27 +0800

    Adjust to use with Androsot main.py

--- image_processing.py
+++ image_processing.py
@@ -5,7 +5,7 @@ import cv2.aruco as aruco
 import math
 from Robot_pos_estimator import my_estimatePoseSingleMarkers,calc_Aruco_distance,rotation_mtx2euler_angle,get_Aruco_information
 from color_masking import colorMasking
-ARUCO_SIZE = 0.08#0.053
+ARUCO_SIZE = 0.078#0.053
 CAMERA_HEIGHT = 1.5
 CAMERA_DISTANCE = 1.2
 FIELD_LENGTH = 3.4
@@ -27,7 +27,7 @@ class Robot():
     
     def robot_pose_estimation(self):
         index = 999
-        print(self.id)
+        # print(self.id)
         if self.id == 0:
             order = [0,6,2,8,4]
         elif self.id == 1:
@@ -43,15 +43,19 @@ class Robot():
                 break
 
         if(index != 999):#,3,5,7,9,counterclockwise
-            print("index",index,"sef",self.aruco_x_list )
+            # print("index",index,"sef",self.aruco_x_list )
             if self.camera_setting == "left":
                 self.x_left = self.aruco_x_list[index] - CAMERA_DISTANCE*100
                 self.y_left = (FIELD_WIDTH * 100 // 2 + self.aruco_y_list[index])
-                self.degree_left = self.aruco_degree_list[index]
+                self.degree_left = degree0to360transform(self.aruco_degree_list[index])
             elif self.camera_setting == "right":
                 self.x_right = FIELD_LENGTH* 100 - (self.aruco_x_list[index] - CAMERA_DISTANCE*100)
                 self.y_right = (FIELD_WIDTH * 100 // 2 - self.aruco_y_list[index])
                 self.degree_right = self.aruco_degree_list[index]
+                self.degree_right = degree0to360transform(self.aruco_degree_list[index])
+                self.degree_right = self.degree_right + 180
+                if self.degree_right >= 360:
+                    self.degree_right = self.degree_right - 360
 
     
     def get_information(self,needed_direction):
@@ -68,7 +72,7 @@ class Robot():
             return [self.x_right,self.y_right],self.degree_right
         
         elif needed_direction == 'overall':
-            self.postion_estimator()
+            self.position_estimator()
             return [self.x,self.y],self.degree
         
     def position_estimator(self):
@@ -120,31 +124,36 @@ class Robot_Team():
         id_result = []
         x_result = []
         y_result = []
+        pos_result = []
         degree_result = []
         for robot in self.Robot_list:
             if needed_direction == "left":
                 print("robot:",robot.id," x:",robot.x_left," y:",robot.y_left," degree",robot.degree_left)
                 id_result.append(robot.id)
-                x_result.append(robot.x_left)
-                y_result.append(robot.y_left)
+                # x_result.append(robot.x_left)
+                # y_result.append(robot.y_left)
+                pos_result.append([robot.x_left,robot.y_left])
                 degree_result.append(robot.degree_left)
             elif needed_direction == "right":
                 print("robot:",robot.id," x:",robot.x_right," y:",robot.y_right," degree",robot.degree_right)
                 id_result.append(robot.id)
-                x_result.append(robot.x_right)
-                y_result.append(robot.y_right)
+                # x_result.append(robot.x_right)
+                # y_result.append(robot.y_right)
+                pos_result.append([robot.x_right,robot.y_right])
                 degree_result.append(robot.degree_right)
                 # return robot.id_right,robot.x_right,robot.y_right,robot.degree_right
 
             elif needed_direction == "overall":
-                print("robot:",robot.id," x:",robot.x," y:",robot.y," degree",robot.degree)
+                robot.get_information(needed_direction)
+                # print("robot:",robot.id," x:",robot.x," y:",robot.y," degree",robot.degree)
                 # return robot.id,robot.x,robot.y,robot.degree
                 id_result.append(robot.id)
-                x_result.append(robot.x)
-                y_result.append(robot.y)
+                # x_result.append(robot.x)
+                # y_result.append(robot.y)
+                pos_result.append([robot.x,robot.y])
                 degree_result.append(robot.degree)
         
-        return id_result,x_result,y_result,degree_result
+        return id_result,pos_result,degree_result
 
 
 
@@ -154,8 +163,9 @@ class Ball():
         self.y = 0
 
     def update_position(self,frame,camera_setting):
-        self.x = 0
-        self.y = 0
+        font = cv2.FONT_HERSHEY_SIMPLEX #font for displaying text (below)
+        # self.x = 0
+        # self.y = 0
         mask = colorMasking(frame,"yellow")
         mask = cv2.medianBlur(mask, 5)  # 中值濾波
         contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # 抓出輪廓
@@ -171,7 +181,8 @@ class Ball():
                 center = [ball_x,ball_y]
                 radius = int(radius)
                 # print(radius,"radius")
-                if radius > 15 and int((ball_y / 1080) * FIELD_WIDTH * 100) < 140:
+                # if radius > 15 and int((ball_y / 1080) * FIELD_WIDTH * 100) < 140:
+                if radius > 15 :
                     cv2.circle(frame, center, radius, (0,255,0), 3)  # 畫圓
                     cv2.putText(frame, "Ball", (x, y - 5), font, 0.7, (0,255,0), 2)  # 螢幕上寫出"Ball"
                     datacenter.append(center)  # 儲存球心座標
@@ -210,11 +221,11 @@ class Camera():
         if self.index == "left":
             # calibration_matrix_path = "./calibration_matrix_left.npy"
             # distortion_coefficients_path = "./distortion_coefficients_left.npy"
-            calibration_matrix_path = "./calibration_matrix.npy"
-            distortion_coefficients_path = "./distortion_coefficients.npy"
+            calibration_matrix_path = "./calibration_matrix_left.npy"
+            distortion_coefficients_path = "./distortion_coefficients_left.npy"
         elif self.index == "right":
-            calibration_matrix_path = "./calibration_matrix.npy"
-            distortion_coefficients_path = "./distortion_coefficients.npy"
+            calibration_matrix_path = "./calibration_matrix_right.npy"
+            distortion_coefficients_path = "./distortion_coefficients_right.npy"
 
         self.camera_matrix = np.load(calibration_matrix_path)
         self.dist_matrix = np.load(distortion_coefficients_path)
@@ -245,7 +256,7 @@ def gamma_correction(frame, gamma=1.0):
  
 class Aruco():
     def __init__(self):
-        self.aruco_dict = cv2.aruco.getPredefinedDictionary(aruco.DICT_4X4_100)
+        self.aruco_dict = cv2.aruco.getPredefinedDictionary(aruco.DICT_5X5_100)
         self.parameters =  cv2.aruco.DetectorParameters()
         self.parameters.perspectiveRemoveIgnoredMarginPerCell = 0.3
         self.parameters.adaptiveThreshWinSizeMin = 3
@@ -257,10 +268,39 @@ class Aruco():
         #使用aruco.detectMarkers()函數可以檢測到marker，返回ID和标志板的4个角点坐標
         corners, ids, rejectedImgPoints = self.detector.detectMarkers(gray)
         return corners,ids,rejectedImgPoints
+
+def degree2unit_vector(angle_deg):
+    angle_rad = math.radians(angle_deg)
+    x_component = math.cos(angle_rad)
+    y_component = math.sin(angle_rad)
+
+    # Normalize the components to get the unit vector
+    length = math.hypot(x_component, y_component)
+    unit_vector = [x_component / length, y_component / length]
+    # print("length == ",length)
+    return unit_vector
     
-if __name__ == '__main__':
-    camera1 = Camera("left",number=0,video_path="WIN_20240327_19_20_56_Pro.mp4")
-    camera2 = Camera("right",number=1,video_path="2024-03-27_19-20-54.mp4")
+def degree0to360transform(ori_degree):
+    if ori_degree >= 0:
+        return ori_degree
+    elif ori_degree < 0:
+        
+        new_degree = 360 + ori_degree
+        return new_degree
+    
+
+        
+team_degree = []
+team_pos = []
+oppo_pos = []
+oppo_degree = []
+ball_center = []
+
+def image_result():
+    # camera1 = Camera("left",number=0,video_path="WIN_20240327_19_20_56_Pro.mp4")
+    # camera2 = Camera("right",number=1,video_path="2024-03-27_19-20-54.mp4")
+    camera1 = Camera("left",number=0,video_path=False)
+    camera2 = Camera("right",number=2,video_path=False)
     font = cv2.FONT_HERSHEY_SIMPLEX #font for displaying text (below)
     red_team = Robot_Team(color='red',length = 2)
     blue_team = Robot_Team(color='blue',length = 2)
@@ -269,7 +309,7 @@ if __name__ == '__main__':
     camera_list = [camera1,camera2]
     while True:
         for camera in camera_list:
-            print(camera.index)
+            # print(camera.index)
             # if camera.index == "right":
             #     break
             # result = frame_run(camera)
@@ -280,8 +320,8 @@ if __name__ == '__main__':
             ret, frame = cap.read()
             h1, w1 = frame.shape[:2]
             #糾正畸變
-            newcameramtx, roi = cv2.getOptimalNewCameraMatrix(camera1.camera_matrix, camera1.dist_matrix, (h1, w1), 1, (h1, w1))
-            # dst1 = cv2.undistort(frame, camera_matrix, dist_matrix, None, newcameramtx)
+            newcameramtx, roi = cv2.getOptimalNewCameraMatrix(camera.camera_matrix, camera.dist_matrix, (h1, w1), 1, (h1, w1))
+            # dst1 = cv2.undistort(frame, camera.camera_matrix, camera.dist_matrix, None, newcameramtx)
             # x, y, w1, h1 = roi
             # dst1 = dst1[y:y + h1, x:x + w1]
             # frame = dst1
@@ -299,7 +339,7 @@ if __name__ == '__main__':
             corners,ids,rejectedImgPoints = aruco_define.update(frame = frame)
 
             frame,id_list,distance_list,distance_x_list,degree_list,degree_list_x,degree_list_y,degree_list_z\
-                = get_Aruco_information(camera_matrix=camera1.camera_matrix,dist_matrix=camera1.dist_matrix,frame=frame,corners=corners,ids=ids)
+                = get_Aruco_information(camera_matrix=camera.camera_matrix,dist_matrix=camera.dist_matrix,frame=frame,corners=corners,ids=ids)
             # print("dis",distance_list,distance_x_list)
             red_id_list,blue_id_list,red_x_list,blue_x_list,red_y_list,blue_y_list,red_degree_list,blue_degree_list = [],[],[],[],[],[],[],[]
 
@@ -322,24 +362,70 @@ if __name__ == '__main__':
             blue_team.update_position(blue_id_list,blue_x_list,blue_y_list,blue_degree_list,camera_setting=camera.index)
             red_team.update_position(red_id_list,red_x_list,red_y_list,red_degree_list,camera_setting=camera.index)
 
-            red_team.get_information(needed_direction=camera.index)
-            id_result,x_result,y_result,degree_result = blue_team.get_information(needed_direction=camera.index)
+            # red_team.get_information(needed_direction=camera.index)
+            id_result_blue,pos_blue,degree_result_blue = blue_team.get_information(needed_direction=camera.index)
+            id_result_red,pos_red,degree_result_red = red_team.get_information(needed_direction=camera.index)
 
             # frame_resized = cv2.resize(frame, (0, 0), fx=1000, fy=800)
             frame = cv2.warpPerspective(frame, m, (1080, 1920))
             if camera.index == "left":
                 frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
             elif camera.index =="right":
+                # pass
                 frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
             ball.update_position(frame=frame,camera_setting=camera.index)
-            cv2.putText(frame,'ids:'+ str(id_result) ,(0, 70), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
-            cv2.putText(frame,'coordinates:'+ str([x_result,y_result]) ,(0, 110), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
-            cv2.putText(frame,'deg_z:'+ str(degree_result) ,(0, 150), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
-            cv2.putText(frame,'ballcenter:'+ str([ball.x,ball.y]) ,(0, 190), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+            transform_degree_red = []
+            transform_degree_blue = []
+            for degree in degree_result_blue:
+                # degree = degree0to360transform(degree)
+                degree = degree2unit_vector(degree)
+                transform_degree_blue.append(degree)
+
+            for degree in degree_result_red:
+                # degree = degree0to360transform(degree)
+                degree = degree2unit_vector(degree)
+                transform_degree_red.append(degree)
+
+            cv2.putText(frame,'Blue:' ,(0, 30), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+            cv2.putText(frame,'ids:'+ str(id_result_blue) ,(0, 70), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+            cv2.putText(frame,'coordinates:'+ str(pos_blue) ,(0, 110), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+            cv2.putText(frame,'deg_z:'+ str(degree_result_blue) ,(0, 150), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+            # cv2.putText(frame,'ballcenter:'+ str([ball.x,ball.y]) ,(0, 190), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+
+            cv2.putText(frame,'Red:' ,(0, 230), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+            cv2.putText(frame,'ids:'+ str(id_result_red) ,(0, 270), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+            cv2.putText(frame,'coordinates:'+ str(pos_red) ,(0, 310), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+            cv2.putText(frame,'deg_z:'+ str(degree_result_red) ,(0, 350), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+            cv2.putText(frame,'ballcenter:'+ str([ball.x,ball.y]) ,(0, 390), font, 1, (0, 255, 0), 2,cv2.LINE_AA)
+            
             cv2.namedWindow('frame '+ camera.index, cv2.WINDOW_NORMAL)
             cv2.resizeWindow('frame ' + camera.index, 600, 400)
             cv2.imshow('frame ' + camera.index,frame)
+
+        id_result_blue,pos_blue,degree_result_blue = blue_team.get_information(needed_direction="overall")
+        id_result_red,pos_red,degree_result_red = red_team.get_information(needed_direction="overall")
+
+        transform_degree_red = []
+        transform_degree_blue = []
+        for degree in degree_result_blue:
+            # degree = degree0to360transform(degree)
+            degree = degree2unit_vector(degree)
+            transform_degree_blue.append(degree)
+
+        for degree in degree_result_red:
+            # degree = degree0to360transform(degree)
+            degree = degree2unit_vector(degree)
+            transform_degree_red.append(degree)
+
+        global team_degree,team_pos,oppo_pos,oppo_degree,ball_center
+        print("team_pos_im",team_pos)
+        print("pos_blue",pos_blue)
+        team_degree = transform_degree_blue
+        team_pos = pos_blue
+        oppo_pos = pos_red
+        oppo_degree = transform_degree_red
+
 
         key = cv2.waitKey(1)
 
@@ -352,6 +438,27 @@ if __name__ == '__main__':
         if key == ord(' '):
             filename = str(time.time())[:10] + ".jpg"
             cv2.imwrite(filename, frame)
+
+if __name__ == '__main__':
+    image_result()
+
+
+
+        # transform_degree = []
+        # for degree in degree_result_blue:
+        #     degree = degree0to360transform(degree)
+        #     degree = degree2unit_vector(degree)
+        #     transform_degree.append(degree)
+
+        # for degree in degree_result_red:
+        #     degree = degree0to360transform(degree)
+        #     degree = degree2unit_vector(degree)
+        #     transform_degree.append(degree)
+
+
+    
+
+
 
         
 
